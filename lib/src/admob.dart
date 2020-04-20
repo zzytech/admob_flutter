3628e73fb560c668b6d15812c3fc6c9f4ec0b874
v7lin <v7lin@qq.com>
2020-04-20 11:04:55 +0800

    admob 中介测试套件

--- lib/src/admob.dart
+++ lib/src/admob.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/services.dart';
 
 class Admob {
-  Admob.initialize(String appId) {
-    MethodChannel _channel = const MethodChannel('admob_flutter');
-    _channel.invokeMethod('initialize', appId);
+  Admob._();
+
+  static const MethodChannel _channel = MethodChannel('admob_flutter');
+
+  static Future<void> initialize(String appId) {
+    return _channel.invokeMethod('initialize', appId);
+  }
+
+  static Future<void> launchTestSuite({String testDevice}) {
+    return _channel.invokeMethod(
+      'launchTestSuite',
+      (testDevice?.isNotEmpty ?? false) ? <String, dynamic>{
+        'testDevice': testDevice
+      } : <String, dynamic>{},
+    );
   }
 }
